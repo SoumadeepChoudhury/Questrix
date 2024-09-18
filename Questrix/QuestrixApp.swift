8e75217fe79027ebe07ae11168e604037ab1e905
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2024-09-18 12:46:17 +0530

    Initial Commit

---
+++ Questrix/QuestrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestrixApp.swift
+//  Questrix
+//
+//  Created by Soumadeep Choudhury on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuestrixTests/QuestrixTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuestrixTests.swift
+//  QuestrixTests
+//
+//  Created by Soumadeep Choudhury on 18/09/24.
+//
+
+import Testing
+@testable import Questrix
+
+struct QuestrixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
